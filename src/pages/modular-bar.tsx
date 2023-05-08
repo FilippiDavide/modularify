7eba7f79007606b1a728b1621ef5854b9dd5bf11
FilippiDavide <61542774+FilippiDavide@users.noreply.github.com>
2023-05-08 16:35:20 +0200

    unity app is now responsive

--- src/pages/modular-bar.tsx
+++ src/pages/modular-bar.tsx
@@ -1,28 +1,50 @@
-import {Anchor, Box, Center, Group, Stack, Text} from "@mantine/core";
+import {Anchor, Box, Center, Group, MediaQuery, Stack, Text} from "@mantine/core";
 import {IconDownload} from "@tabler/icons-react";
 
 const modularBar = () =>{
     return(
-        <Box pl={'5%'} pr={'5%'}>
-            <div style={{height:" 75vh", backgroundColor: "#ffffff", borderRadius:'22px', width:'100%'}} >
-                <Center h='100%'>
-                    <embed type="text/html" src="https://modularify.github.io/ProgressBars3D/"  width="80%" height="100%"/>
-                </Center>
-            </div>
-            <Group position="right" mt={'1%'}>
-                <div>
-                    <Stack spacing={0}>
-                        <Text fw={700} fz={'30px'} variant="gradient" gradient={{ from: 'brand.0', to: 'brand.1', deg: 45 }}>Modular 3d Progress Bars</Text>
+        <>
+            <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+                <Box pl={'5%'} pr={'5%'}>
+                    <div style={{height:" 75vh", backgroundColor: "#ffffff", borderRadius:'22px', width:'100%'}} >
+                        <Center h='100%'>
+                            <embed type="text/html" src="https://modularify.github.io/ProgressBars3D/"  width="80%" height="100%"/>
+                        </Center>
+                    </div>
+                    <Group position="right" mt={'1%'}>
                         <div>
-
-                            <Anchor href="/Modularify_ProgressBarDocumentation.pdf" target="_blank" color='white.1'>
-                                <Group position="left"><IconDownload size={16}/>Modularify doc</Group>
-                            </Anchor>
+                            <Stack spacing={0}>
+                                <Text fw={700} fz={'30px'} variant="gradient" gradient={{ from: 'brand.0', to: 'brand.1', deg: 45 }}>Modular 3d Progress Bars</Text>
+                                <div>
+                                    <Anchor href="/Modularify_ProgressBarDocumentation.pdf" target="_blank" color='white.1'>
+                                        <Group position="left"><IconDownload size={16}/>Modularify doc</Group>
+                                    </Anchor>
+                                </div>
+                            </Stack>
                         </div>
-                    </Stack>
-                </div>
-            </Group>
-        </Box>
+                    </Group>
+                </Box>
+            </MediaQuery>
+            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+                <Box>
+                    <Center h='100%'>
+                        <embed type="text/html" src="https://modularify.github.io/ProgressBars3D/" height="200px" width="100%"/>
+                    </Center>
+                    <Group position="right" mt={'1%'}>
+                        <div>
+                            <Stack spacing={0}>
+                                <Text fw={700} fz={'30px'} variant="gradient" gradient={{ from: 'brand.0', to: 'brand.1', deg: 45 }}>Modular 3d Progress Bars</Text>
+                                <div>
+                                    <Anchor href="/Modularify_ProgressBarDocumentation.pdf" target="_blank" color='white.1'>
+                                        <Group position="left"><IconDownload size={16}/>Modularify doc</Group>
+                                    </Anchor>
+                                </div>
+                            </Stack>
+                        </div>
+                    </Group>
+                </Box>
+            </MediaQuery>
+        </>
     );
 }
 export default modularBar;
